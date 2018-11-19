61265aee673e3af95a2e33da80e536e16c29d9c2
Jesús Redondo García <redondogarciajesus@gmail.com>
2018-11-19 13:12:10 +0100

    Fixed CORS for PUT

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ mongoose.connect('mongodb://localhost/ClaseTaskdb');
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
   next();
 });
 
